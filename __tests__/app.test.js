9b26829edfb52d1259dce4f16c71f9c1dceb768d
Annalise Murphy <nali247@gmail.com>
2020-11-18 10:33:09 -0800

    main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+// const Router = require('../lib/controllers/auth-controller');
 
 describe('Mood-Music-be routes', () => {
-  it('redirects user with token to front page', async() => {
+  it('/login - fart fart test', async() => {
     const response = await request(app)
       .get('/login');
 
     expect(response.body).toEqual(expect.any(Object));
   });
   
-
 });
